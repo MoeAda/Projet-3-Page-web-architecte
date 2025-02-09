8d1dd6e7804c237444a1380342586838e21e652e
Morgane Legond <l.morgane@live.fr>
2025-02-09 18:57:45 +0100

    test de récupération du mail et mot de passe en JS

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,1 +1,10 @@
-console.log("Toto")
+let connexion = document.getElementById("login")
+document.addEventListener("submit", function (event) {
+    let baliseMail = document.getElementById("mail")
+    let mail = baliseMail.value
+    console.log(mail)
+
+let balisePassword = document.getElementById("password")
+    let password = balisePassword.value
+    console.log(password)      
+})
