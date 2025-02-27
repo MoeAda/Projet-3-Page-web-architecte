9b6f0a5837934031c9ed7d717b8444835f68f465
Morgane Legond <l.morgane@live.fr>
2025-02-27 10:49:04 +0100

    Mise en place des filtres

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -9,84 +9,61 @@ if (token) {
   logout.style.display = "block";
 }
 
-/*
-function categories() {
-  fetch("http://localhost:5678/api/categories", function (response) {})
-    .then(function (response) {
-      if(response.ok){
-        return response.json();
-      }
-    })
-    .then(function (data) {
-      console.log(data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-
-categories();
-
-*/
-
 async function getCategories() {
   try {
     const response = await fetch("http://localhost:5678/api/categories");
     const data = await response.json();
 
-    const filters = document.getElementById("categories-picture");
-    const all = document.createElement("option"); // <option></option>
-    all.value = 0;
-    all.innerText = 'Tous';
-    filters.appendChild(all);
+    const filters = document.getElementById("categories");
+    const button = document.createElement("button");
+    button.type = "button";
+    button.className = "btn-styled";
+    button.innerText = "Tous";
+    button.value = 0;
+    filters.appendChild(button);
 
     data.forEach((item) => {
-      const option = document.createElement("option");
-      option.value = item.id;
-      option.textContent = item.name;
-      filters.appendChild(option);
+      const categorieButton = document.createElement("button");
+      categorieButton.type = "button";
+      categorieButton.className = "btn-styled";
+      categorieButton.value = item.id;
+      categorieButton.innerText = item.name;
+      filters.appendChild(categorieButton);
+    });
 
-   
-
-      // 1 : Créer un eventListener sur filters (Change)
-      /// ....addEventListener... 
-      // 2 : Afficher dans un console.log la catégorie (ID) souhaité
-      // console.log(catégorie : 3) / toto = 3
-      // 3 : Envoyer l'information dans la fonction getWorks (Ajout d'un params ( ne pas oublier le params par défaut))
-      // getWorks($categoryId)
-      // 4 : Dans getWorks trier les résultats en fonction de la catégories souhaitée
-   
-
-
-
+    filters.addEventListener("click", (event) => {
+      console.log("ID de la catégorie :", event.target.value);
+      // "Reload" les données
+      getWorks(event.target.value);
     });
   } catch (error) {
     console.log(error);
   }
 }
 
-async function getWorks(categoryId = 0) {
+
+async function getWorks(categoryId = "0") {
   try {
     const response = await fetch("http://localhost:5678/api/works");
     const data = await response.json();
-    const gallery = document.getElementsByClassName("gallery");
+    const gallery = document.querySelector("#gallery");
+    gallery.innerHTML = "";
+    data
+      .filter((item) => item.categoryId.toString() === categoryId || categoryId === "0")
+      .forEach((item) => {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
 
-    console.log(data);
+        img.setAttribute("src", item.imageUrl);
+        img.setAttribute("alt", item.title);
 
-    data.forEach((item) => {
-      const figure = document.createElement("figure");
-      const img = document.createElement("img");
-      const figcaption = document.createElement("figcaption");
+        figcaption.innerText = item.title;
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
 
-      img.setAttribute("src", item.imageUrl);
-      img.setAttribute("alt", item.title);
-
-      figcaption.innerText = item.title;
-      figure.appendChild(img);
-      figure.appendChild(figcaption);
-
-      gallery[0].appendChild(figure);
-    });
+        gallery.appendChild(figure);
+      });
   } catch (error) {
     console.log(error);
   }
@@ -94,3 +71,10 @@ async function getWorks(categoryId = 0) {
 
 getCategories();
 getWorks();
+
+// Types de variable
+// Nombre ? Chaine de caractères ? Objet ? Tableaux ? Booléan
+// let maVariable: String = "Ma chaine de caractères" Dans les langagues typé statiquement, le type de la variable ne peut pas changer
+// JS est typé dynamiquement donc on peut changer "à la volée" le type de ma variable 
+// === : On check d'abord le type pour il compare les valeurs si c'est les mêmes types, sinon retourne false directement
+// == : On s'en fout du type pq il se dérouille tout seul pour faire matcher les types. JS trouve un truc en commun pour pouvoir traduire. ex : 2 == "2" ? -> "2" == "2"; true == "true" -> "true" == "true". À chaque fois (ou presque) JS transforme en chaine de caractères.
